213101180ba7a84a4f6ec8c637530203bbcebce4
Brooke Meleah <brooke.meleah@fluxfederation.com>
2017-12-21 22:46:03 +1300

    working character search

--- characters.rb
+++ characters.rb
@@ -1,30 +1,42 @@
 require 'google_drive'
+require 'byebug'
 
 #character list
 class Characters
   def initialize
     session = GoogleDrive::Session.from_config("config.json")
     @list = session.spreadsheet_by_key("1nB9d4KpnpgvIUm63JyOJ3YR2BNG92yx6KTho9CeoI5I").worksheets[0]
-    find('Glass')
+    reload
   end
 
   def keys
-    puts "hi"
+    puts 'generating keys...'
     key_list = []
-    (2..@list.num_cols).each do |row|
+    (2..@list.num_rows).each do |row|
       key_list.push(@list[row, 1])
     end
     key_list
   end
 
   def find(search)
-    names = keys
-    names.map { |name|
-      if name.include?(search)
+    reload
+
+    puts "searching for #{search}..."
+    req = search.join(' ')
+    names = keys.map(&:downcase)
+
+    out = []
+    names.map do |name|
+      if name.include?(req.downcase)
         name_key = names.index(name) + 2
-        puts @list[name_key, 2]
+        out.push(@list[name_key, 2])
       end
-    }
+    end
+    out.join(' ')
+  end
+
+  def reload
+    @list.reload
   end
 end
 
--- pinebot.rb
+++ pinebot.rb
@@ -3,20 +3,16 @@ require 'google_drive'
 require_relative 'quote_module'
 require_relative 'characters'
 
-# Here we instantiate a `CommandBot` instead of a regular `Bot`, which has the functionality to add commands using the
-# `command` method. We have to set a `prefix` here, which will be the character that triggers command execution.
+
 bot = Discordrb::Commands::CommandBot.new token: 'Mzg2Mzk5NTY1MDIyMjMyNTc2.DRxnpA.r480IIQmfLa6YYZKKEmf_RvHyg8', prefix: '~'
+@search = Characters.new
+
 
 bot.command :user do |event|
-  # Commands send whatever is returned from the block to the channel. This allows for compact commands like this,
-  # but you have to be aware of this so you don't accidentally return something you didn't intend to.
-  # To prevent the return value to be sent to the channel, you can just return `nil`.
   event.user.name
 end
 
 bot.command(:random, min_args: 0, max_args: 2, description: 'Generates a random number between 0 and 1, 0 and max or min and max.', usage: 'random [min/max] [max]') do |_event, min, max|
-  # The `if` statement returns one of multiple different things based on the condition. Its return value
-  # is then returned from the block and sent to the channel
   if max
     rand(min.to_i..max.to_i)
   elsif min
@@ -43,11 +39,19 @@ bot.command :template do |event|
   event.user.pm('```' + template + '```')
 end
 
+bot.command :character do |_event, *args|
+  @search.find(args)
+end
+
 bot.message(content: 'Ping!') do |event|
-  # The `respond` method returns a `Message` object, which is stored in a variable `m`. The `edit` method is then called
-  # to edit the message with the time difference between when the event was received and after the message was sent.
   m = event.respond('Pong!')
   m.edit "Pong! Time taken: #{Time.now - event.timestamp} seconds."
+end
+
+bot.command(:exit, help_available: false) do |event|
+  break unless event.user.id == 98308488941309952 # Replace number with your ID
+  bot.send_message(event.channel.id, 'dead now.')
+  exit
 end
 
 bot.run
